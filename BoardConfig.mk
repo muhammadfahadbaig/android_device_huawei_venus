de9b2b93de9eb8bfe693ee068cb181785069530e
Muhammad Fahad Baig <muhammad.fb.79@gmail.com>
2016-09-10 18:55:24 +0500

    venus: small fixes for TWRP

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -69,3 +69,7 @@ TWHAVE_SELINUX := true
 TW_BRIGHTNESS_PATH := "/sys/devices/platform/hisi_fb.1048577/leds/lcd_backlight0/brightness"
 TW_CUSTOM_BATTERY_PATH := "/sys/devices/battery.5/power_supply/Battery"
 BOARD_HAS_NO_SELECT_BUTTON := true
+TW_INTERNAL_STORAGE_PATH := "/data/media/0"
+TW_INTERNAL_STORAGE_MOUNT_POINT := "sdcard"
+TW_EXTERNAL_STORAGE_PATH := "/external_sd"
+TW_EXTERNAL_STORAGE_MOUNT_POINT := "external_sd"
